5f2748575bbbb6e374dfe6b5a2aa71cc25b9c783
aninarafath6 <arafathanin6.com>
2021-06-07 11:13:08 +0530

    initial commit ;

---
+++ lib/home.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'facebook clone',
+        home: Scaffold(
+          body: Container(),
+        ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:facebook_clone/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Home());
+}
